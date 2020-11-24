1aaaccdd91e62b52ad0ef65593a6600a4b5e5ea9
unknown <ashkanshojaee48@gmail.com>
2020-11-24 13:44:06 +0330

    Updates

---
+++ precache-manifest.4f00c8e510207a59dbc78fb38c5ebbb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34bf88051077880f32a8",
+    "url": "/Todo-Mobx-React/static/css/main.5d881533.chunk.css"
+  },
+  {
+    "revision": "34bf88051077880f32a8",
+    "url": "/Todo-Mobx-React/static/js/main.34bf8805.chunk.js"
+  },
+  {
+    "revision": "fbc947128e2cc343645e",
+    "url": "/Todo-Mobx-React/static/js/1.fbc94712.chunk.js"
+  },
+  {
+    "revision": "ed333ac6290b0d509506",
+    "url": "/Todo-Mobx-React/static/js/runtime~main.ed333ac6.js"
+  },
+  {
+    "revision": "6a314c47745c5c609b33ce8486bdf690",
+    "url": "/Todo-Mobx-React/index.html"
+  }
+];
